fdf5f8292b9ad7b2178ff9f0fd8e66035614799e
JuanCas2409 <99415542+JuanCas2409@users.noreply.github.com>
2022-02-10 16:41:28 -0600

    Examen 1 POO
    Commit 1

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.Examen1.jcc;
+
+public class Main {
+	
+	static void main (String[] args)
+	{
+		Biblioteca bib1 = new Biblioteca("1234", "Biblioteca Benito Juarez", "Av Adolfo Lopez Mateos", 1920);
+		
+	}
+
+}
