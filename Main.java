a74ea8f6d7be61b530b191e9eb54b5ccdfbef45c
JuanCas2409 <99415542+JuanCas2409@users.noreply.github.com>
2022-02-10 17:15:27 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,11 @@ public class Main {
 	static void main (String[] args)
 	{
 		Biblioteca bib1 = new Biblioteca("1234", "Biblioteca Benito Juarez", "Av Adolfo Lopez Mateos", 1920);
+		bib1.nuevoeprofesor("Juan","Castillo", 20, "H");
+		bib1.nuevoeprofesor("Pepe", "Pistolas", 30, "H");
+		bib1.nuevoestudiante("Mandy", "Frisch", 13, "M");
+		
+		
 		
 	}
 
